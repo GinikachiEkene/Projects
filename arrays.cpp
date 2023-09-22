3825dec698c8814c7b0b3b69ff85c105475a2bcb
Ekene Ginikachi <95496949+GinikachiEkene@users.noreply.github.com>
2023-09-23 00:34:01 +0100

    Add files via upload
    This c++ program asks for a number of even numbers to be counted, and using the 'for' loop, it lists a group of even numbers up to the specified number to be counted.

---
+++ arrays.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int even[5];
+	int num = 2;
+	int mult = 1;
+	int many; //Number of even numbers that is to be printed out.
+
+	std::cout << "Good day, lets count the even numbers together.\n";
+	std::cout << "How many even numbers would you like us to count?\n";
+	std::cin >> many;
+	std::cout << "\n\n";
+	std::cout << "Okay then, that's " << many << " sequential even numbers.\n\n";
+	std::cout << "The listed even numbers are: "; 
+	for(int i = 0; i < many; i++)
+	{
+		even[i] = num * mult;
+		mult++;
+		std::cout << even[i] << ", ";
+	} 
+
+	std::cout << "\n\n";
+return 0;
+}
